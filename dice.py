10d84975507018e656ada3d3fd7b7204dc51d06e
SophieOrgana <sophie@googlemail.io>
2020-01-02 23:16:34 +0000

    removed some return statements

--- dice.py
+++ dice.py
@@ -12,21 +12,21 @@ def roll(n, sides):
 
 dice = roll(n, sides)
 
-if sides = 6:
+if sides == 6:
     for x in dice:
-        if roll = 1:
+        if roll == 1:
             score = (0,0)
-        elif roll = 2:
+        elif roll == 2:
             score = (0,0)
-        elif roll = 3:
+        elif roll == 3:
             score = (1,0)
-        elif roll = 4:
+        elif roll == 4:
             score = (1,1)
-        elif roll = 5:
+        elif roll == 5:
             score = (0,2)
-        elif roll = 6:
+        elif roll == 6:
             score = (0,1)
-return(score)
+            # return(score)
 
 print(dice, sum(dice))
 print(score)
