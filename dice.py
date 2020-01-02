d5c96e7ef537a74fb1987a5141b84fd231c60196
SophieOrgana <sophie@googlemail.io>
2020-01-02 23:44:19 +0000

    removed added score for d6/boost dice

--- dice.py
+++ dice.py
@@ -2,7 +2,7 @@ from random import randint
 
 sides=int(input("Input the number of sides of the die you want to roll:"))
 n=int(input("Input the number of dice you want to roll:"))
-score=()
+
 
 def d(sides):
     return randint(1, sides)
@@ -12,20 +12,23 @@ def roll(n, sides):
 
 dice = roll(n, sides)
 
-if sides == 6:
-    for x in dice:
-        if roll == 1:
-            score = (0,0)
-        elif roll == 2:
-            score = (0,0)
-        elif roll == 3:
-            score = (1,0)
-        elif roll == 4:
-            score = (1,1)
-        elif roll == 5:
-            score = (0,2)
-        elif roll == 6:
-            score = (0,1)
+# def score(dice):
+#     if sides == 6:
+dscore=[]
+for x in dice:
+    if x == 1:
+        dscore.append([0,0])
+    elif x == 2:
+        dscore.append([0,0])
+    elif x == 3:
+        dscore.append([1,0])
+    elif x == 4:
+        dscore.append([1,1])
+    elif x == 5:
+        dscore.append([0,2])
+    elif x == 6:
+        dscore.append([0,1])
+        # return(dscore)
 
 print(dice, sum(dice))
-print(score)
+print(dscore)
