fc8422bf02b41da8c10a6246b2b45a1ec886f6ab
Sophie <53999238+SophieOrgana@users.noreply.github.com>
2020-01-03 13:22:55 -0500

    added comments to the code

--- eotedice.py
+++ eotedice.py
@@ -5,7 +5,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
-
+# dictionary for each type of die and their sides
+# the position in the lists corresponds to a pip on the physical dice
 dice = {
     "boost": [[0,0],[0,0],[1,0],[1,1],[0,2],[0,1]],
     "setback": [[0,0],[0,0],[-1,0],[-1,0],[0,-1],[0,-1]],
@@ -14,6 +15,8 @@ dice = {
     "proficiency": [[0,0],[1,0],[1,0],[2,0],[2,0],[0,1],[1,1],[1,1],[1,1],[0,2],[0,2],[1,1,1]],
     "challenge": [[0,0],[-1,0],[-1,0],[-2,0],[-2,0],[0,-1],[0,-1],[-1,-1],[-1,-1],[0,-2],[0,-2],[-1,-1,-1]]
 }
+
+##This is outdated code that was used to test that the dice would roll properly
 # for i, x in enumerate(dice["proficiency"]):
 #     # print(i)
 #     y = dice["challenge"][i]
@@ -21,6 +24,7 @@ dice = {
 #     score = [x[0]+y[0],x[1]+y[1]]
 #     print(score)
 
+# This code is used to get the dice pool from the front end and post the results
 @app.route('/rolldie', methods=['POST'])
 def postroll():
     data = request.get_json()
