a685043d60dbffc3ea2000a87ddcac6f149eb6e2
SophieOrgana <admin@sophiesolo.biz>
2020-01-09 16:08:36 -0500

    Completed interactive rolling section

--- static/main.js
+++ static/main.js
@@ -5,7 +5,6 @@ function toCssId(name) {
 }
 
 for (diceElement of $(dice.map(toCssId).join())) {
-    const die = diceElement.id;
     const input = diceElement.querySelector('.dice');
     const minusButton = diceElement.querySelector('.minus');
     const plusButton = diceElement.querySelector('.plus');
@@ -37,3 +36,63 @@ for (diceElement of $(dice.map(toCssId).join())) {
         input.value = String(Math.max(0, num));
     })
 }
+
+aggregateOrig = $('#aggregate-roll').html();
+totalOrig = $('#total-roll').html();
+
+const clear = $("#clear")[0]
+clear.addEventListener('click', ev => {
+    for (diceElement of $(dice.map(toCssId).join())) {
+        diceElement.querySelector('.dice').value = "0";
+    }
+    $('#total-roll').html(totalOrig);
+    $('#aggregate-roll').html(aggregateOrig);
+});
+
+function renderDice(response, diceList) {
+    const individualRolls = diceList.map((die, i) => [die, response.individual_rolls[i]]);
+    $('#total-roll').html(individualRolls.map(([dice, res]) => `<img src="/static/dice/${getFileName(dice, res)}" alt="" />`).join('\n'));
+}
+
+function renderAggregate(response) {
+    console.log(response);
+    const pips = [];
+    const sum = response.sum
+    for (let i = 0; i < sum.length; i++) {
+        console.log('loop1');
+        const amount = Math.abs(sum[i]);
+        let index = Math.sign(sum[i]);
+        if (index === 0) continue;
+        index = Math.max(index, 0);
+        for (let j = 0; j < amount; j++) {
+            console.log('loop2');
+            pips.push(names[i][index])
+        }
+    }
+    console.log('pips:', pips)
+    $('#aggregate-roll').html(pips.map(pip =>
+        `<img src="/static/pips/${pip}.svg" alt="" />`
+    ).join('\n'));
+}
+
+const rollDice = $("#rollDice")[0]
+rollDice.addEventListener('click', ev => {
+    const diceList = [];
+    for (diceElement of $(dice.map(toCssId).join())) {
+        const die = diceElement.id;
+        const amount = Number.parseInt(diceElement.querySelector('.dice').value);
+        for (let i = 0; i < amount; i++) {
+            diceList.push(die)
+        }
+    }
+    const ret = $.ajax("/rolldie", {
+        data: JSON.stringify(diceList),
+        dataType: "json",
+        method: "POST",
+        contentType: "application/json"
+    }).then(response => {
+        renderDice(response, diceList);
+
+        renderAggregate(response);
+    });
+});
