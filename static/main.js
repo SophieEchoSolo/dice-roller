d64d347b8158dd1e21084cab0db83c30b219bb18
SophieOrgana <admin@sophiesolo.biz>
2020-01-09 14:52:31 -0500

    Added event listeners to dice inputs

--- static/main.js
+++ static/main.js
@@ -6,5 +6,34 @@ function toCssId(name) {
 
 for (diceElement of $(dice.map(toCssId).join())) {
     const die = diceElement.id;
-
+    const input = diceElement.querySelector('.dice');
+    const minusButton = diceElement.querySelector('.minus');
+    const plusButton = diceElement.querySelector('.plus');
+    input.value = 0;
+    input.addEventListener('keypress', ev => {
+        if (!ev.key.match(/^\d$/))
+            ev.preventDefault();
+    });
+    input.addEventListener('paste', ev => {
+        ev.preventDefault();
+    });
+    input.addEventListener('blur', ev => {
+        const el = ev.target;
+        const num = Number.parseInt(el.value);
+        if (isNaN(num)) {
+            el.value = "0";
+            return;
+        }
+        el.value = String(Math.max(0, num));
+    });
+    minusButton.addEventListener('click', ev => {
+        let num = Number.parseInt(input.value);
+        num -= 1;
+        input.value = String(Math.max(0, num));
+    })
+    plusButton.addEventListener('click', ev => {
+        let num = Number.parseInt(input.value);
+        num += 1;
+        input.value = String(Math.max(0, num));
+    })
 }
